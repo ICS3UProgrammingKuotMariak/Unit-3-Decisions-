fc4a3d5ac28336021586ce9718d2d2be7b32a6b8
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2018-10-15 10:53:27 -0400

    Added-RockPaperScissors

--- RockPaperScissors(KuotM)/RockPaperScissors(KuotM)/RockPaperScissorsForm.Designer.cs
+++ RockPaperScissors(KuotM)/RockPaperScissors(KuotM)/RockPaperScissorsForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RockPaperScissors_KuotM_
 {
-    partial class Form1
+    partial class RockPaperScissorsForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // RockPaperScissorsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(339, 298);
+            this.Name = "RockPaperScissorsForm";
+            this.Text = "Rock Paper Scissors!";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- RockPaperScissors(KuotM)/RockPaperScissors(KuotM)/RockPaperScissorsForm.cs
+++ RockPaperScissors(KuotM)/RockPaperScissors(KuotM)/RockPaperScissorsForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RockPaperScissors_KuotM_
 {
-    public partial class Form1 : Form
+    public partial class RockPaperScissorsForm : Form
     {
-        public Form1()
+        public RockPaperScissorsForm()
         {
             InitializeComponent();
         }
