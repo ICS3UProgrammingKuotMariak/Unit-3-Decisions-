3f84fc0dee21fca8ddacb5ad0bfbf7a033e18903
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2018-10-29 11:48:42 -0400

    RandomNumber

---
+++ RandomNumber(KuotM)/RandomNumber(KuotM)/RandomNumberForm.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace RandomNumber_KuotM_
+{
+    partial class RandomNumberForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudGuess = new System.Windows.Forms.NumericUpDown();
+            this.lblGuessNumber = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblStateAnswer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudGuess)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nudGuess
+            // 
+            this.nudGuess.Location = new System.Drawing.Point(255, 91);
+            this.nudGuess.Name = "nudGuess";
+            this.nudGuess.Size = new System.Drawing.Size(120, 20);
+            this.nudGuess.TabIndex = 0;
+            // 
+            // lblGuessNumber
+            // 
+            this.lblGuessNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuessNumber.Location = new System.Drawing.Point(12, 70);
+            this.lblGuessNumber.Name = "lblGuessNumber";
+            this.lblGuessNumber.Size = new System.Drawing.Size(219, 54);
+            this.lblGuessNumber.TabIndex = 1;
+            this.lblGuessNumber.Text = "Guess a number between 1 and 10";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(410, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(116, 144);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(164, 59);
+            this.btnCheck.TabIndex = 3;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblStateAnswer
+            // 
+            this.lblStateAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStateAnswer.Location = new System.Drawing.Point(65, 228);
+            this.lblStateAnswer.Name = "lblStateAnswer";
+            this.lblStateAnswer.Size = new System.Drawing.Size(249, 38);
+            this.lblStateAnswer.TabIndex = 4;
+            this.lblStateAnswer.Text = "Answer";
+            this.lblStateAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // RandomNumberForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 301);
+            this.Controls.Add(this.lblStateAnswer);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.lblGuessNumber);
+            this.Controls.Add(this.nudGuess);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "RandomNumberForm";
+            this.Text = "Random Number";
+            this.Load += new System.EventHandler(this.RandomNumberForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudGuess)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudGuess;
+        private System.Windows.Forms.Label lblGuessNumber;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblStateAnswer;
+    }
+}
+
---
+++ RandomNumber(KuotM)/RandomNumber(KuotM)/RandomNumberForm.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Created by: Kuot Mariak
+ * Created on: 11-10-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #17 - Random Number
+ * Random Number
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumber_KuotM_
+{
+    public partial class RandomNumberForm : Form
+    {
+        public RandomNumberForm()
+        {
+            InitializeComponent();
+            // Declares the constants and variables
+            int userChoice;
+            int correctAnswer;
+            const int Min_Value = 1;
+            const int Max_Value = 10;
+
+            // This generates a random number
+            Random randomNumberGenerator = new Random();
+            correctAnswer = randomNumberGenerator.Next(Min_Value, Max_Value + 1);
+            
+            
+        }
+
+        private void RandomNumberForm_Load(object sender, EventArgs e)
+        {
+            lblStateAnswer.Hide();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+
+            // This converts the NUD into an int
+            userChoice = (int)(nudGuess.Value);
+
+            // This sets the min and max values
+            nudGuess.Minimum = 1;
+            nudGuess.Maximum = 10;
+
+
+            if (userChoice == correctAnswer)
+            {
+                lblStateAnswer.Show();
+                lblStateAnswer.Text = Convert.ToString("You win!");
+            }
+            else
+            {
+                lblStateAnswer.Show();
+                lblStateAnswer.Text = Convert.ToString("You lose");
+            }
+
+            
+           
+
+           
+        }
+    }
+}
