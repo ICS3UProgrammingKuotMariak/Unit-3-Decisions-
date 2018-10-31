fe8efc7abf9b39fbdbfa181c310a5f91279fedd3
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2018-10-31 10:57:07 -0400

    FactorialDoWhile

--- FactorialDoWhile(KuotM)/FactorialDoWhile(KuotM)/FactorialDoWhileForm.Designer.cs
+++ FactorialDoWhile(KuotM)/FactorialDoWhile(KuotM)/FactorialDoWhileForm.Designer.cs
@@ -31,7 +31,12 @@
             this.mnuFactorialDoWhile = new System.Windows.Forms.MenuStrip();
             this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
             this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
-            this.lstFactorial = new System.Windows.Forms.ListBox();
+            this.lstFactorialNumbers = new System.Windows.Forms.ListBox();
+            this.lblEnterNum = new System.Windows.Forms.Label();
+            this.txtUserNum = new System.Windows.Forms.TextBox();
+            this.lblFactorialAnswer = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblMultiplyNum = new System.Windows.Forms.Label();
             this.mnuFactorialDoWhile.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -58,21 +63,72 @@
             this.mniExit.Name = "mniExit";
             this.mniExit.Size = new System.Drawing.Size(152, 22);
             this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
             // 
-            // lstFactorial
+            // lstFactorialNumbers
             // 
-            this.lstFactorial.FormattingEnabled = true;
-            this.lstFactorial.Location = new System.Drawing.Point(22, 76);
-            this.lstFactorial.Name = "lstFactorial";
-            this.lstFactorial.Size = new System.Drawing.Size(381, 238);
-            this.lstFactorial.TabIndex = 1;
+            this.lstFactorialNumbers.FormattingEnabled = true;
+            this.lstFactorialNumbers.Location = new System.Drawing.Point(142, 123);
+            this.lstFactorialNumbers.Name = "lstFactorialNumbers";
+            this.lstFactorialNumbers.Size = new System.Drawing.Size(264, 186);
+            this.lstFactorialNumbers.TabIndex = 1;
+            // 
+            // lblEnterNum
+            // 
+            this.lblEnterNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEnterNum.Location = new System.Drawing.Point(22, 44);
+            this.lblEnterNum.Name = "lblEnterNum";
+            this.lblEnterNum.Size = new System.Drawing.Size(152, 29);
+            this.lblEnterNum.TabIndex = 2;
+            this.lblEnterNum.Text = "Enter a number:";
+            // 
+            // txtUserNum
+            // 
+            this.txtUserNum.Location = new System.Drawing.Point(213, 49);
+            this.txtUserNum.Name = "txtUserNum";
+            this.txtUserNum.Size = new System.Drawing.Size(148, 20);
+            this.txtUserNum.TabIndex = 3;
+            // 
+            // lblFactorialAnswer
+            // 
+            this.lblFactorialAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFactorialAnswer.Location = new System.Drawing.Point(22, 326);
+            this.lblFactorialAnswer.Name = "lblFactorialAnswer";
+            this.lblFactorialAnswer.Size = new System.Drawing.Size(394, 30);
+            this.lblFactorialAnswer.TabIndex = 4;
+            this.lblFactorialAnswer.Text = "Factorial Answer";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(142, 76);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 37);
+            this.btnStart.TabIndex = 5;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblMultiplyNum
+            // 
+            this.lblMultiplyNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMultiplyNum.Location = new System.Drawing.Point(12, 123);
+            this.lblMultiplyNum.Name = "lblMultiplyNum";
+            this.lblMultiplyNum.Size = new System.Drawing.Size(124, 84);
+            this.lblMultiplyNum.TabIndex = 6;
+            this.lblMultiplyNum.Text = "Multiply these numbers together:";
             // 
             // FactorialDoWhileForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(428, 365);
-            this.Controls.Add(this.lstFactorial);
+            this.Controls.Add(this.lblMultiplyNum);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblFactorialAnswer);
+            this.Controls.Add(this.txtUserNum);
+            this.Controls.Add(this.lblEnterNum);
+            this.Controls.Add(this.lstFactorialNumbers);
             this.Controls.Add(this.mnuFactorialDoWhile);
             this.MainMenuStrip = this.mnuFactorialDoWhile;
             this.Name = "FactorialDoWhileForm";
@@ -90,7 +146,12 @@
         private System.Windows.Forms.MenuStrip mnuFactorialDoWhile;
         private System.Windows.Forms.ToolStripMenuItem mniFile;
         private System.Windows.Forms.ToolStripMenuItem mniExit;
-        private System.Windows.Forms.ListBox lstFactorial;
+        private System.Windows.Forms.ListBox lstFactorialNumbers;
+        private System.Windows.Forms.Label lblEnterNum;
+        private System.Windows.Forms.TextBox txtUserNum;
+        private System.Windows.Forms.Label lblFactorialAnswer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblMultiplyNum;
     }
 }
 
--- FactorialDoWhile(KuotM)/FactorialDoWhile(KuotM)/FactorialDoWhileForm.cs
+++ FactorialDoWhile(KuotM)/FactorialDoWhile(KuotM)/FactorialDoWhileForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Kuot Mariak
+ * Created on: 22-10-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #22 - FactorialDoWhile
+ * FactorialDoWhile
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +26,49 @@ namespace FactorialDoWhile_KuotM_
 
         private void FactorialDoWhileForm_Load(object sender, EventArgs e)
         {
+            lblFactorialAnswer.Hide();
+        }
 
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // This declares the local variables
+            double factorialAnswer;
+            double factorialNumber;
+            int factorialCounter;
+
+            // This clears the list box
+            lstFactorialNumbers.Items.Clear();
+
+            // Initializes final answer to 1
+            factorialAnswer = 1;
+
+            // Converts the number from the text box into an int
+            factorialNumber = Convert.ToDouble(txtUserNum.Text);
+
+            // Sets the counter to 0
+            factorialCounter = 0;
+
+            do
+            {
+                // This increments the counter
+                factorialCounter++;
+
+                // This displays the counter number in the list box so the user can see
+                this.lstFactorialNumbers.Items.Add(factorialCounter);
+
+                // This multiplies the counter by the answer
+                factorialAnswer = factorialAnswer * factorialCounter;
+
+                // This shows the label
+                lblFactorialAnswer.Show();
+
+            } while (factorialCounter < factorialNumber);
+            this.lblFactorialAnswer.Text = this.txtUserNum.Text + "! = " + Convert.ToString(factorialAnswer);
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
