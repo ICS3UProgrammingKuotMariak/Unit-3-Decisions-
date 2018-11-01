adc855bb9377ce058e277c19eb484458e81f7bf5
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2018-11-01 10:55:09 -0400

    CatchUp

---
+++ FactorialForLoop/FactorialForLoop/FactorialForForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace FactorialForLoop
+{
+    partial class frmFactorialFor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmFactorialFor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "frmFactorialFor";
+            this.Text = "Factorial For Loop";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ PerfectSquares(KuotM)/PerfectSquares(KuotM)/PerfectSquaresForm.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by: Kuot Mariak
+ * Created on: 24-10-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #22 - Perfect Squares with while loop
+ * Perfect Squares
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquares_KuotM_
+{
+    public partial class frmPerfectSquares : Form
+    {
+        public frmPerfectSquares()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
