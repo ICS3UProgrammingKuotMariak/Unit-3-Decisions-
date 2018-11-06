015a49700a165dfef8bb35c0ab79906e5fb74fa6
KuotMariak <43444982+KuotMariak@users.noreply.github.com>
2018-11-06 11:18:54 -0500

    For Each done

--- ForEachControlObjects/ForEachControlObjects/ControlObjectsForm.Designer.cs
+++ ForEachControlObjects/ForEachControlObjects/ControlObjectsForm.Designer.cs
@@ -28,35 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnColour = new System.Windows.Forms.Button();
-            this.lblColour = new System.Windows.Forms.Label();
+            this.lblChangeVisibility = new System.Windows.Forms.Label();
+            this.lstChangeFont = new System.Windows.Forms.ListBox();
+            this.btnChangeColour = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // btnColour
+            // lblChangeVisibility
             // 
-            this.btnColour.Location = new System.Drawing.Point(106, 65);
-            this.btnColour.Name = "btnColour";
-            this.btnColour.Size = new System.Drawing.Size(140, 73);
-            this.btnColour.TabIndex = 0;
-            this.btnColour.Text = "Change Colour";
-            this.btnColour.UseVisualStyleBackColor = true;
+            this.lblChangeVisibility.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChangeVisibility.Location = new System.Drawing.Point(205, 45);
+            this.lblChangeVisibility.Name = "lblChangeVisibility";
+            this.lblChangeVisibility.Size = new System.Drawing.Size(164, 79);
+            this.lblChangeVisibility.TabIndex = 1;
+            this.lblChangeVisibility.Text = "Change Text";
+            this.lblChangeVisibility.Click += new System.EventHandler(this.lblChangeText_Click);
             // 
-            // lblColour
+            // lstChangeFont
             // 
-            this.lblColour.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblColour.Location = new System.Drawing.Point(102, 240);
-            this.lblColour.Name = "lblColour";
-            this.lblColour.Size = new System.Drawing.Size(139, 29);
-            this.lblColour.TabIndex = 1;
-            this.lblColour.Text = "Change Colour";
+            this.lstChangeFont.FormattingEnabled = true;
+            this.lstChangeFont.Location = new System.Drawing.Point(12, 166);
+            this.lstChangeFont.Name = "lstChangeFont";
+            this.lstChangeFont.Size = new System.Drawing.Size(247, 147);
+            this.lstChangeFont.TabIndex = 2;
+            this.lstChangeFont.Tag = "";
+            this.lstChangeFont.SelectedIndexChanged += new System.EventHandler(this.lstChangeFont_SelectedIndexChanged);
+            // 
+            // btnChangeColour
+            // 
+            this.btnChangeColour.Location = new System.Drawing.Point(12, 12);
+            this.btnChangeColour.Name = "btnChangeColour";
+            this.btnChangeColour.Size = new System.Drawing.Size(178, 112);
+            this.btnChangeColour.TabIndex = 3;
+            this.btnChangeColour.Text = "Change Colour";
+            this.btnChangeColour.UseVisualStyleBackColor = true;
+            this.btnChangeColour.Click += new System.EventHandler(this.btnChangeColour_Click);
             // 
             // frmControlObjects
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(381, 325);
-            this.Controls.Add(this.lblColour);
-            this.Controls.Add(this.btnColour);
+            this.Controls.Add(this.btnChangeColour);
+            this.Controls.Add(this.lstChangeFont);
+            this.Controls.Add(this.lblChangeVisibility);
             this.Name = "frmControlObjects";
             this.Text = "Control Objects";
             this.Load += new System.EventHandler(this.frmControlObjects_Load);
@@ -65,9 +79,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnColour;
-        private System.Windows.Forms.Label lblColour;
+        private System.Windows.Forms.Label lblChangeVisibility;
+        private System.Windows.Forms.ListBox lstChangeFont;
+        private System.Windows.Forms.Button btnChangeColour;
     }
 }
 
--- ForEachControlObjects/ForEachControlObjects/ControlObjectsForm.cs
+++ ForEachControlObjects/ForEachControlObjects/ControlObjectsForm.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Created by: Kuot Mariak
+ * Created on: 2-11-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Control Objects
+ * Control Objects
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +19,8 @@ namespace ForEachControlObjects
 {
     public partial class frmControlObjects : Form
     {
+        Font Sans = new Font("Comic Sans MS", 20, FontStyle.Italic);
+
         public frmControlObjects()
         {
             InitializeComponent();
@@ -22,9 +31,43 @@ namespace ForEachControlObjects
 
         }
 
-        private void btnColour_Click(object sender, EventArgs e)
+        private void lblChangeText_Click(object sender, EventArgs e)
+        {
+            foreach (Control FontControl in this.Controls)
+            {
+                FontControl.Font = Sans;
+                if (FontControl.GetType() == typeof(ListBox))
+                {
+                    FontControl.Font = Sans;
+                }
+                else if (FontControl.GetType() == typeof(Button))
+                {
+                    FontControl.Font = Sans;
+                }
+ 
+            }
+        }
+
+        private void lstChangeFont_SelectedIndexChanged(object sender, EventArgs e)
         {
             
+        }
+
+        private void btnChangeColour_Click(object sender, EventArgs e)
+        {
+            foreach (Control ColourControl in this.Controls)
+            {
+                ColourControl.BackColor = Color.Green;
+
+                if (ColourControl.GetType() == typeof(Label))
+                {
+                    ColourControl.BackColor = Color.Purple;
+                }
+                else if (ColourControl.GetType() == typeof(ListBox))
+                {
+                    ColourControl.BackColor = Color.Orange;
+                }
+            }
         }
     }
 }
